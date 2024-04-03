e46eab544fccd271fd0645680dca75d00d1ccf05
Dar P <darlenegraceq@gmail.com>
2024-04-04 00:44:45 +0800

    feat: Add corresponding pics for OverviewCard

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import OverviewCard from './OverviewCard';
+import gt from "../assets/gt.png";
+import lifg from "../assets/lifg.png";
+import lifp from "../assets/lifp.png";
+import typ from "../assets/typ.png";
 
 const Overview = () => {
   return (
@@ -8,10 +12,10 @@ const Overview = () => {
             <p className="text-4xl text-header font-bold">Overview</p>
             <p className="text-md text-center text-body">some projects i worked on/ contributed to</p>
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 p-8">
-                <OverviewCard title="TyphooNews" summary="A crisis information dashboard system using feedback-based text classification of typhoon-related tweets in the Philippines"/>
-                <OverviewCard title="Lif Game" summary="A play-to-earn platform highlighting user-created games and empowering earnings through play."/>
-                <OverviewCard title="Get Tweets" summary="A web application that scrapes tweets, analyzes sentiment, and visualizes the results."/>
-                <OverviewCard title="Lif Posts" summary="An AI-driven web application that features document editing and article creation"/>
+                <OverviewCard icon={typ} title="TyphooNews" summary="A crisis information dashboard system using feedback-based text classification of typhoon-related tweets in the Philippines"/>
+                <OverviewCard icon={lifg} title="Lif Game" summary="A play-to-earn platform highlighting user-created games and empowering earnings through play."/>
+                <OverviewCard icon={gt} title="Get Tweets" summary="A web application that scrapes tweets, analyzes sentiment, and visualizes the results."/>
+                <OverviewCard icon={lifp} title="Lif Posts" summary="An AI-driven web application that features document editing and article creation"/>
             </div>
         </div>
     </div>
--- src/components/OverviewCard.jsx
+++ src/components/OverviewCard.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import projectIcon from "../assets/projectIcon.png";
 
 const OverviewCard = (props) => {
-    const { title, summary } = props;
+    const { title, summary, icon } = props;
   return (
     <div className="flex flex-col block rounded-lg bg-bg_white shadow-md max-w-96">
-      <img className="rounded-t-lg shadow-none" src={projectIcon} alt="Project Icon" />
+      <img className="rounded-t-lg shadow-none" src={icon} alt="Project Icon" />
       <div className="p-4">
         <h5 className="text-lg font-medium leading-tight text-cardTitle">{title}</h5>
         <p className="text-sm text-cardBody">{summary}</p>
