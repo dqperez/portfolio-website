1d014a617a4ca6a23940d09ac3ccc6a37e93c346
Dar P <darlenegraceq@gmail.com>
2024-04-10 01:55:23 +0800

    fix: Change quote font to serif

--- src/components/GradientQuote.jsx
+++ src/components/GradientQuote.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const GradientQuote = () => {
   return (
     <div className="flex h-auto px-8 lg:px-48">
-        <div class="flex flex items-center justify-center h-24 w-screen p-20 rounded-lg bg-gradient-to-r from-footer to-button">
+        <div class="flex flex font-serif items-center justify-center h-24 w-screen p-20 rounded-lg bg-gradient-to-r from-footer to-button">
                 <p className="text-sm text-center lg:text-lg" style={{ color: 'white' }}>
                     “The simplest way to achieve simplicity is through thoughtful reduction.”
                     <br />— John Maeda            
