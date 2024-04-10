2f9a42c0b462d08281a3362b445a4d32366492bf
Dar P <darlenegraceq@gmail.com>
2024-04-11 01:20:47 +0800

    feat: Content changes

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -4,18 +4,21 @@ import gt from "../assets/gt.png";
 import lifg from "../assets/lifg.png";
 import lifp from "../assets/lifp.png";
 import typ from "../assets/typ.png";
+import dp from "../assets/dp.png";
 
 const Overview = ({workRef}) => {
   return (
     <div ref={workRef} id="work" className="flex w-screen justify-center pt-24">
         <div className="flex flex-col items-center">
             <p className="text-4xl text-header font-bold">Work</p>
-            <p className="text-sm text-center text-body">some projects i worked on/ contributed to</p>
+            <p className="text-sm text-center text-body">some projects i worked on/ contributed to </p>
+            <p className="text-sm text-header">(wip: pages for project details)</p>
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 p-8">
-                <OverviewCard icon={typ} title="TyphooNews" summary="A crisis information dashboard system using feedback-based text classification of typhoon-related tweets in the Philippines"/>
-                <OverviewCard icon={lifg} title="Lif Game" summary="A play-to-earn platform highlighting user-created games and empowering earnings through play."/>
-                <OverviewCard icon={gt} title="Get Tweets" summary="A web application that scrapes tweets, analyzes sentiment, and visualizes the results."/>
-                <OverviewCard icon={lifp} title="Lif Posts" summary="An AI-driven web application that features document editing and article creation"/>
+                <OverviewCard icon={typ} title="TyphooNews" type="PYTHON DJANGO | VUE.JS | MYSQL | VUETIFY"summary="A crisis information dashboard system using feedback-based text classification of typhoon-related tweets in the Philippines"/>
+                <OverviewCard icon={lifg} title="Lif Game" type="FIGMA | PYTHON DJANGO | BOOTSTRAP"summary="A play-to-earn platform highlighting user-created games and empowering earnings through play."/>
+                <OverviewCard icon={gt} title="Get Tweets" type="PYTHON DJANGO | BOOTSTRAP" summary="A web application that scrapes tweets, analyzes sentiment, and visualizes the results."/>
+                <OverviewCard icon={dp} title="My Portfolio" type="FIGMA | REACT.JS | TAILWIND CSS" summary="An AI-driven web application that features document editing and article creation"/>
+                {/* <OverviewCard icon={lifp} title="Lif Posts" type="FIGMA" summary="An AI-driven web application that features document editing and article creation"/> */}
             </div>
         </div>
     </div>
--- src/components/OverviewCard.jsx
+++ src/components/OverviewCard.jsx
@@ -2,22 +2,23 @@ import React from 'react'
 import projectIcon from "../assets/projectIcon.png";
 
 const OverviewCard = (props) => {
-    const { title, summary, icon } = props;
+    const { title, type, summary, icon } = props;
     
   return (
     <div className="flex flex-col block rounded-lg bg-bg_white shadow-md max-w-96">
-      <img className="rounded-t-lg shadow-none" src={icon} alt="Project Icon" />
-      <div className="p-4 h-24">
+      <img className="rounded-t-lg shadow-none p-2" src={icon} alt="Project Icon" />
+      <div className="p-4 h-30">
         <h5 className="text-md font-bold leading-tight text-cardTitle">{title}</h5>
-        <p className="text-sm text-cardBody">{summary}</p>
+        <p className="text-xs text-footer font-bold">{type}</p>
+        <p className="text-sm text-cardBody py-2">{summary}</p>
       </div>
 
-      <div className="flex flex-col p-4">
+      {/* <div className="flex flex-col p-4">
         <div role="button"
           className="px-3 w-max flex gap-2 items-center rounded-full bg-footer text-bg_white hover:bg-opacity-75">
           <span class="block text-xs">UI/UX</span>
         </div> 
-      </div>  
+      </div>   */}
 
      
 </div>
