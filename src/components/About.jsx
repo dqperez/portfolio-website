e5e6dbfd5d903721c2272b6fc424e8e4175411cc
Dar P <darlenegraceq@gmail.com>
2024-04-11 01:27:07 +0800

    feat: Changed about me text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,8 +10,8 @@ const About = ({aboutRef}) => {
 
       <div className="w-1/2 lg:w-1/3 lg:p-16">
         <p className="text-4xl text-header font-bold">About me</p>
-        <p className="text-body text-sm my-4">
-        Hello again! I graduated from the University of the Philippines Manila with a bachelor’s degree in Computer Science. I’m currently working as a software developer, with tasks mostly focused on front-end development and web design. 
+        <p className="text-body text-md my-4">
+        Hello again! I completed my degree in BS Computer Science at UP Manila. I’m currently working as a software developer, with tasks mostly focused on front-end development and web design. 
           <br/><br/>
           I see programming as a field full of endless opportunities for learning and problem-solving. It keeps me engaged and allows me to explore my creativity by merging tech with design. On my free time, you can find me playing competitive or cozy games, creating digital artworks, and finding the best place for matcha.
         </p>
